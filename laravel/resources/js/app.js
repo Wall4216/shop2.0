48255def027a79969d5e4701785a7e99571a2eba
Wall <ilnazamirhanov61@gmail.com>
2023-01-08 14:53:40 +0300

    correction app.js

--- laravel/resources/js/app.js
+++ laravel/resources/js/app.js
@@ -1,1 +1,8 @@
 import './bootstrap';
+
+import Vue from 'vue'
+Vue.component('app', require('/laravel/vendor/laravel/ui/src/Presets/vue-stubs/ExampleComponent.vue').default);
+
+const app = new Vue({
+    el: '#app',
+})
